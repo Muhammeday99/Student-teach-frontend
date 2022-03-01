746f4f810b82b0b30e486a2e6d8635c411a23562
Ammar-64 <ammar.n.sammour@gmail.com>
2022-03-01 15:47:45 +0300

    fixed the navbar alignment, embedded figma files, added other technologiesand added team cards with gihub links

--- src/components/design.js
+++ src/components/design.js
@@ -11,7 +11,7 @@ const SysDesign = () => {
         <iframe
           style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
           width="75%"
-          height="450"
+          height="650"
           src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FNCGmFkvvEq00ZLgalAsrB3%2FSystem-Design%3Fnode-id%3D0%253A1"
         ></iframe>
       </div>
